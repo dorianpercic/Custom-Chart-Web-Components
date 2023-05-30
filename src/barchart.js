633f696207ccd613c17087c6c90419e450edac05
Dorian Percic <dorian.percic@gmail.com>
2023-05-30 13:25:01 +0200

    Implement table functionality, multi series missing though

--- src/barchart.js
+++ src/barchart.js
@@ -3,6 +3,7 @@ class BarChart extends HTMLElement {
     super();
     this.attachShadow({ mode: 'open' });
   }
+
   connectedCallback() {
     if (this.querySelector('table')) {
       this.handleTableMode();
@@ -14,7 +15,7 @@ class BarChart extends HTMLElement {
   }
 
   /**
-   * Function for drawing the bar chart usind data series.
+   * Function for drawing the chart usind data series.
    *
    *
    *
@@ -31,7 +32,7 @@ class BarChart extends HTMLElement {
   }
 
   /**
-   * Function for drawing the bar chart in table mode.
+   * Function for drawing the chart in table mode.
    *
    *
    *
@@ -48,7 +49,7 @@ class BarChart extends HTMLElement {
   }
 
   /**
-   * Function drawing the bar chart and adding to the Shadow DOM.
+   * Function drawing the chart and adding to the Shadow DOM.
    * @param {number} width: Width of chart
    * @param {number} height: Height of chart
    * @param {map} dataDict: Array of data points
@@ -65,7 +66,7 @@ class BarChart extends HTMLElement {
     const chartWidth = width - margin.left - margin.right;
     const chartHeight = height - margin.top - margin.bottom;
 
-    const barChartSvg = d3
+    const chartSvg = d3
       .create('svg')
       .attr('viewBox', `0 0 ${width} ${height}`)
       .attr('version', '1.1');
@@ -84,18 +85,17 @@ class BarChart extends HTMLElement {
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale).ticks(5);
 
-    barChartSvg
+    chartSvg
       .append('g')
       .attr('transform', `translate(${margin.left}, ${height - margin.bottom})`)
       .call(xAxis);
 
-    barChartSvg
+    chartSvg
       .append('g')
       .attr('transform', `translate(${margin.left}, ${margin.top})`)
       .call(yAxis);
 
-    // Draw rectangles of bar-chart
-    barChartSvg
+    chartSvg
       .append('g')
       .selectAll('rect')
       .data(
@@ -111,7 +111,7 @@ class BarChart extends HTMLElement {
       .attr('fill', 'red');
 
     // X-Axis
-    barChartSvg
+    chartSvg
       .append('text')
       .attr('class', 'x-label')
       .attr('text-anchor', 'end')
@@ -120,7 +120,7 @@ class BarChart extends HTMLElement {
       .text('Custom title');
 
     // Y-Axis
-    barChartSvg
+    chartSvg
       .append('text')
       .attr('class', 'y-label')
       .attr('text-anchor', 'end')
@@ -130,29 +130,15 @@ class BarChart extends HTMLElement {
       .attr('transform', 'rotate(-90)')
       .text('Value');
 
-    this.shadowRoot.appendChild(barChartSvg.node());
+    this.shadowRoot.appendChild(chartSvg.node());
   }
 
   /**
-   * Function, which creates the chart dictionary.
-   * @param {object} dataPointElements: Datapoints are stored here.
-   * @returns {map}: Return map of data points: key -> label(string), value -> value(number).
+   * Function, which creates the chart dictionary out of data series.
+   *
+   * @returns {map}: Return map of data points: key -> label(string), value -> number.
    */
   getDataSeriesDict() {
-    const isValidStructure = Array.from(this.children).every(
-      (child) =>
-        child.nodeName.toLowerCase() === 'data-series' &&
-        Array.from(child.children).every(
-          (grandchild) => grandchild.nodeName.toLowerCase() === 'data-point'
-        )
-    );
-
-    if (!isValidStructure) {
-      throw new Error(
-        'Invalid structure: The correct structure is: <bar-chart> <data-series> <data-point>...</data-point> </data-series> </bar-chart>'
-      );
-    }
-
     const dataSeriesElement = this.querySelector('data-series');
 
     if (!dataSeriesElement) {
@@ -168,13 +154,11 @@ class BarChart extends HTMLElement {
     dataPointElements.forEach((dataPoint) => {
       let dataPointInnerHtml = dataPoint.innerHTML.replace(/\s/g, '');
       let dataValues = dataPointInnerHtml.split(',');
-      if (!dataValues[0] || !dataValues[1]) {
-        throw new Error(
-          `<data-point> value ${dataPointInnerHtml}cannot be parsed`
-        );
+      if (!dataValues[0]) {
+        throw new Error(`<data-point> value is missing`);
       } else if (!this.isValidNumber(dataValues[0])) {
         throw new Error(
-          `<data-point> value ${dataValues[0]}is not a valid input for bar chart`
+          `<data-point> value ${dataValues[0]} is not a valid input for chart`
         );
       }
       dataDict[dataValues[1]] = parseFloat(dataValues[0]);
@@ -198,7 +182,7 @@ class BarChart extends HTMLElement {
   /**
    * Function setting the chart width.
    *
-   * @returns {number}: Return width of bar chart. Default set to 1000.
+   * @returns {number}: Return width of chart. Default set to 1000.
    */
   setChartWidth() {
     let width = 1000;
@@ -215,7 +199,7 @@ class BarChart extends HTMLElement {
   /**
    * Function setting the chart height.
    *
-   * @returns {number}: Return height of bar chart. Default set to 250.
+   * @returns {number}: Return height of chart. Default set to 250.
    */
   setChartHeight() {
     let containsLettersRegex = /[a-zA-Z]/g;
@@ -235,11 +219,46 @@ class BarChart extends HTMLElement {
    * @returns {boolean}: Return true if string contains only numbers, else false.
    */
   isValidNumber(num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
+    return !isNaN(parseFloat(num)) && isFinite(num) && parseFloat(num) >= 0;
   }
 
+  /**
+   * Function, which creates the chart dictionary out of input table.
+   *
+   * @returns {map}: Return map of data points: key -> label(string), value -> number.
+   */
   getTableDict() {
-    return null;
+    let dataDict = {};
+    let tableBody = this.querySelector('table').querySelector('tbody');
+
+    if (!tableBody) {
+      throw new Error('<tbody> element not found');
+    }
+
+    const trElements = tableBody.querySelectorAll('tr');
+    if (!trElements.length) {
+      throw new Error('No <tr> elements found inside <tbody>');
+    }
+    for (const row of trElements) {
+      const td = row.querySelectorAll('td');
+      if (td.length < 2) {
+        throw new Error('<td> elements have to be > 1');
+      }
+      const key = td[0].textContent.trim();
+
+      if (!td[1].textContent) {
+        throw new Error(`<td> value is missing`);
+      } else if (!this.isValidNumber(td[1].textContent)) {
+        throw new Error(
+          `<td> value ${td[1].textContent} is not a valid input for chart`
+        );
+      }
+      const value = parseFloat(td[1].textContent);
+
+      dataDict[key] = value;
+    }
+    console.log(dataDict);
+    return dataDict;
   }
 }
 
